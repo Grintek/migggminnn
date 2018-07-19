64c3bb50f9ffedd940f93e4ad23a4816740f319e
Grintek <grisha2@bk.ru>
2018-07-19 11:36:54 +0300

    comit

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -20,8 +20,26 @@
     </section>
 
 
-    <hr style="color: #2a88bd">
     <div class="navbar line"></div>
 
-    
+    <section class="row new-post">
+        <div class="col-lg-6 col-md-offset-3">
+            <header><h4>Tut sozdaetsy vash kanal i td i tp</h4></header>
+            <hr>
+            <div class="navbar line2"></div>
+
+            <form action="{{ route('admin') }}" method="post">
+                <header><h3>заголовок канала</h3></header>
+                <input type="text" class="form-control" name="caption_chan" id="new_channel">
+                <hr>
+                <hr>
+                <header><h3>Описание</h3></header>
+                <textarea class="form-control" style="padding-bottom: 200px;" name="descript_chan"></textarea>
+                <button type="submit" class="btn btn-primary">Save Account</button>
+                <input type="hidden" value="{{ Session::token() }}" name="_token">
+            </form>
+        </div>
+
+    </section>
+
 @endsection
