7c0e0dac2e38052439f912b0aaf2c4191a395b8e
Grisha <grisha2@bk.ru>
2018-11-18 18:40:33 +0300

    Revert:comMeet
    Signed-off-by: Grisha <grisha2@bk.ru>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,12 @@
         <div>
             @foreach($channel as $ch)
                 <button onclick="window.location='{{route('channel.vk.id',[$ch->vk_id])}}'" style="background: url({{ route('accountedit.image',
-                ['filename' => $ch->caption_chan . '-' . $ch->vk_id . '.jpg']) }}) 50% 50%; background-size: cover;" id="channel-{{$ch->vk_id}}" class="container_channel">
+                ['filename' => $ch->caption_chan . '-' . $ch->vk_id . '.jpg']) }}) 50% 50%; background-size: cover;" id="channel-{{$ch->vk_id}}" class=
+                @if($ch->offOn_channel === 0)
+                        "container_channel">
+                @else
+                        "container_channel_play">
+                @endif
                 </button>
             @endforeach
         </div>
